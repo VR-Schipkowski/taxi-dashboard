0c77fb0c9727f11c32fae1c3ee1076a033f53430
Vincent <vincent.ridder@tuhh.de>
2026-06-15 14:02:18 +0200

    added new topic to kafka

---
+++ init_topic.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+/opt/kafka/bin/kafka-topics.sh --create --if-not-exists \
+  --topic taxi-locations \
+  --bootstrap-server kafka:9092 \
+  --partitions 1 --replication-factor 1
+
+/opt/kafka/bin/kafka-topics.sh --create --if-not-exists \
+  --topic taxi-speeding-alerts \
+  --bootstrap-server kafka:9092 \
+  --partitions 1 --replication-factor 1
