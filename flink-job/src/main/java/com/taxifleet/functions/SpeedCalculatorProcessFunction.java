899c4a45cdb3785721267b21475b4d62ee09f7e6
Vincent <vincent.ridder@tuhh.de>
2026-06-15 14:14:19 +0200

    removin unused import

--- flink-job/src/main/java/com/taxifleet/functions/SpeedCalculatorProcessFunction.java
+++ flink-job/src/main/java/com/taxifleet/functions/SpeedCalculatorProcessFunction.java
@@ -8,7 +8,6 @@ import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.util.Collector;
-import org.checkerframework.checker.units.qual.C;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
